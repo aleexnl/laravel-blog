f5b82a65be0d6c8c87cef5400c9e2cd3761bfbb2
aleexnl <alex.nieto0027@gmail.com>
2021-02-18 13:42:10 +0100

    add: comment relationships

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -8,4 +8,18 @@ use Illuminate\Database\Eloquent\Model;
 class Comment extends Model
 {
     use HasFactory;
+    /**
+     * Get the post that owns the comment.
+     */
+    public function post()
+    {
+        return $this->belongsTo(Post::class);
+    }
+    /**
+     * Get the user that owns the comment.
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
