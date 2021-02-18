7690a9662899eb1d2a3b43f7c9f8bd2e887c01b1
aleexnl <alex.nieto0027@gmail.com>
2021-02-18 13:42:16 +0100

    add: likes relationship

--- app/Models/Like.php
+++ app/Models/Like.php
@@ -8,4 +8,18 @@ use Illuminate\Database\Eloquent\Model;
 class Like extends Model
 {
     use HasFactory;
+    /**
+     * Get the post that owns the like.
+     */
+    public function post()
+    {
+        return $this->belongsTo(Post::class);
+    }
+    /**
+     * Get the user that owns the like.
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
