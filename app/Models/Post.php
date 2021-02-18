427abf954bc8c70c59cb0ed190cb177d113712e7
aleexnl <alex.nieto0027@gmail.com>
2021-02-18 13:42:24 +0100

    add: post relationship

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -8,4 +8,25 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
+    /**
+     * Get the comments for the blog post.
+     */
+    public function comments()
+    {
+        return $this->hasMany(Comment::class);
+    }
+    /**
+     * Get the likes for the blog post.
+     */
+    public function likes()
+    {
+        return $this->hasMany(Like::class);
+    }
+    /**
+     * Get the user that owns the post.
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
